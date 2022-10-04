11c95c0a1022697baa895352e573812adf073dab
Anton Kinstler <antonkinstler@gmail.com>
2022-10-04 16:32:28 +0600

    Initial Commit

---
+++ MaximalTest/MaximalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaximalTestApp.swift
+//  MaximalTest
+//
+//  Created by Anton Kinstler on 04.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MaximalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
